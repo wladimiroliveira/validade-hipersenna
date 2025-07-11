762b7709950e656804e332fe87c20200bad6655f
wladimiroliveira <wladimir2020moraes@outlook.com>
2025-07-11 17:18:52 -0300

    update - adiciona novas trativas ao sistema

--- js/relatorio/relatorioDetalhado.js
+++ js/relatorio/relatorioDetalhado.js
@@ -7,6 +7,9 @@ const opcoesTratativa = [
     "⁠Troca com Fornecedor",
     "⁠Transferência Interna",
     "⁠Bloqueio para Venda",
+    "Produto vendável dentro do prazo⁠",
+    "Lançamento tardío⁠",
+    "Vencido⁠",
     "⁠Doação"
 ];
 
